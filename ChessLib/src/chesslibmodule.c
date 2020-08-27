0f60384f1dd74e255b777f285e11f09ae8461ea7
Marco Tröster <marco@troester-gmbh.de>
2020-08-27 15:46:02 +0200

    - restructured project: removed empty main project cmakelists.txt file. everything is now configured in a single cmake file - added distutils python deployment script (still not sure if it should be used in this case or not, commented it anyways)

--- ChessLib/src/chesslibmodule.c
+++ ChessLib/src/chesslibmodule.c
@@ -12,6 +12,24 @@ ChessBoard deserialize_chessboard(PyObject* board);
               P Y T H O N    M O D U L E
    ================================================= */
 
+static PyMethodDef chesslib_methods[] = {
+    {"ChessBoard", chesslib_create_chessboard, METH_VARARGS, "Python interface for creating a new chess board using a C library function"},
+    {"ChessBoard_StartFormation", chesslib_create_chessboard_startformation, METH_VARARGS, "Python interface for creating a new chess board in start formation using a C library function"},
+    {"ChessDraw", chesslib_create_chessdraw, METH_VARARGS, "Python interface for creating a new chess draw using a C library function"},
+    {"ChessPiece", chesslib_create_chesspiece, METH_VARARGS, "Python interface for creating a new chess piece using a C library function"},
+    {"ChessPosition", chesslib_create_chessposition, METH_VARARGS, "Python interface for creating a new chess position using a C library function"},
+    {"ChessPieceAtPos", chesslib_create_chesspieceatpos, METH_VARARGS, "Python interface for creating a new chess piece including its' position using a C library function"},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef chesslib_module = {
+    PyModuleDef_HEAD_INIT,
+    "chesslib",
+    "Python interface for efficient chess draw-gen C library functions",
+    -1,
+    chesslib_methods
+};
+
 PyMODINIT_FUNC PyInit_chesslib(void)
 {
     return PyModule_Create(&chesslib_module);
