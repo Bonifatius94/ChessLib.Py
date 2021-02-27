b8779f65c381ba3a3412a51e93ca2f5fd5df8465
Marco Tröster <marco@troester-gmbh.de>
2021-02-27 23:56:39 +0100

    Update build_manylinux.sh

--- build_manylinux.sh
+++ build_manylinux.sh
@@ -7,11 +7,10 @@
 PYTHON_VERSION=$1
 PYTHON_ROOT=/opt/python/$PYTHON_VERSION/bin
 
-cd /build
-
 # install build dependencies
-$PYTHON_ROOT/pip install -r requirements.txt
+$PYTHON_ROOT/pip install -r /build/requirements.txt
 
 # build manylinux wheels (expects /output directory to exist)
+cd /build
 $PYTHON_ROOT/pip wheel /build -w /output-out
 auditwheel repair /output-out/chesslib*$PYTHON_VERSION*.whl -w /output
